975a93fe8221a12b3f983e31d38e9ab35de54975
François Juteau <fjuteau@veepee.com>
2023-05-13 10:36:55 +0200

    Create project

---
+++ SimpleArchi/SimpleArchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleArchi
+//
+//  Created by François JUTEAU on 13/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
