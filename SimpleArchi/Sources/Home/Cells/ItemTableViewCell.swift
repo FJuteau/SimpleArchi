6ae3d8a4e83e0b4d87e9cb35926879f3595580df
François Juteau <fjuteau@veepee.com>
2023-05-14 14:19:50 +0200

    Working TableView

---
+++ SimpleArchi/Sources/Home/Cells/ItemTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  ItemTableViewCell.swift
+//  SimpleArchi
+//
+//  Created by François JUTEAU on 13/05/2023.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    let label = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        backgroundColor = .random()
+
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(label: String) {
+        self.label.text = label
+    }
+
+    private func setupLayout() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        let constraints = [
+            label.leftAnchor.constraint(equalTo: leftAnchor),
+            label.rightAnchor.constraint(equalTo: rightAnchor),
+            label.topAnchor.constraint(equalTo: topAnchor),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+}
+
--- SimpleArchi/Sources/Home/HomeViewController.swift
+++ SimpleArchi/Sources/Home/HomeViewController.swift
@@ -7,12 +7,72 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
+
+    private let tableView = UITableView()
+    private let dataSource = HomeTableViewDataSource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = dataSource
+        registerCells()
+        setupLayout()
+    }
 
-        view.backgroundColor = UIColor.yellow
+    private func setupLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        let constraints = [
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        tableView.backgroundColor = .yellow
+        view.backgroundColor = .blue
+    }
+
+    private func registerCells() {
+        tableView.register(ItemTableViewCell.self, forCellReuseIdentifier: ItemTableViewCell.classIdentifier)
+    }
+
+    private func updateList(updatedList: [String]) {
+        dataSource.list = updatedList
+        tableView.reloadData()
     }
 }
 
+final private class HomeTableViewDataSource: NSObject, UITableViewDataSource {
+
+    var list: [String] = [
+        "s1",
+        "s2",
+        "s3",
+        "s4",
+        "s5",
+        "s6",
+        "s7",
+        "s8",
+        "s9",
+    ]
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        list.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: ItemTableViewCell.classIdentifier, for: indexPath) as? ItemTableViewCell
+        else { return UITableViewCell() }
+        cell.configure(label: list[indexPath.row])
+        return cell
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("***** HomeViewController: didSelectRowAt: \(dataSource.list[indexPath.row])")
+    }
+}
